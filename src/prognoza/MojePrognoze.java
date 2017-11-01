f6265c344f662be6b35e240f763c1f0dfc1f0af3
David Abram <david@abram.com>
2017-11-01 21:38:05 +0100

    init

---
+++ src/prognoza/MojePrognoze.java
@@ -0,0 +1,36 @@
+package prognoza;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+public class MojePrognoze {
+	private ObjectFactory objectFactory;
+	private Prognoze prognoze;
+	
+	MojePrognoze() {
+	 this.objectFactory = new ObjectFactory();
+	 this.prognoze = objectFactory.createPrognoze();
+	}
+	
+	void make(XMLGregorianCalendar dohvacena, String dan, XMLGregorianCalendar vrijeme) {
+		Prognoze.Prognoza prognoza = new Prognoze.Prognoza();
+		prognoza.setDohvacena(dohvacena);
+		Prognoze.Prognoza.Datum datum = new Prognoze.Prognoza.Datum();
+		datum.setDan(dan);
+		datum.setVrijeme(vrijeme);
+		prognoza.setDatum(datum);
+		this.prognoze.getPrognoza().add(prognoza);
+	}
+	
+	void marshal() {
+		try {
+			JAXBContext jc = JAXBContext.newInstance("prognoze");
+			Marshaller m = jc.createMarshaller();
+			m.marshal(this.prognoze, System.out );
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+}
